fd0226d054dc1d126f583510aed73acd611a20ab
Michael Harris <mhguitar1997@gmail.com>
2019-11-06 14:09:40 -0800

    Stock prices first pass and its not good

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,12 +4,33 @@ import argparse
 
 def find_max_profit(prices):
   # loop through prices and keep track of current min proce and the max profit
+  # in loop subtract the min price from  item and if items larger than profit change profit
   max_profit_so_far = 0
   current_min_price_so_far = 0
-  num_of_prices = 0
-  while num_of_prices < len(prices):
-    print(num_of_prices)
-    num_of_prices += 1
+  # num_of_prices = 0
+
+  for i in prices:
+    if i == prices[0]:
+      current_min_price_so_far = i
+    if max_profit_so_far <= i - current_min_price_so_far:
+      max_profit_so_far = i - current_min_price_so_far
+      print(f"max {max_profit_so_far}, {current_min_price_so_far}, {i}")
+    if i == prices[-1] and max_profit_so_far == 0:
+      print(f"last item {i}, {current_min_price_so_far}")
+      max_profit_so_far = i - current_min_price_so_far
+    if i < current_min_price_so_far:
+      current_min_price_so_far = i
+
+    print(f"min - {current_min_price_so_far}, max - {max_profit_so_far}, i - {i}")
+  return max_profit_so_far
+    # print(f"i is {i}, current min price is {current_min_price_so_far}")
+  # while num_of_prices < len(prices) - 1:
+  #   print(prices[num_of_prices], current_min_price_so_far)
+  #   current_min_price_so_far = prices[num_of_prices]
+  #   if prices[num_of_prices] < current_min_price_so_far:
+  #     current_min_price_so_far = prices[num_of_prices]
+  #   print("min price", prices[num_of_prices], current_min_price_so_far)
+  #   num_of_prices += 1
 
 
 if __name__ == '__main__':
