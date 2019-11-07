ad0972cb9902e5a45bf93e53f1ef2f64ae9e907b
Michael Harris <mhguitar1997@gmail.com>
2019-11-07 14:10:02 -0800

    Debugging recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -13,19 +13,34 @@ def recipe_batches(recipe, ingredients):
   #
   batches = 0
   print(recipe.items())
-  for ingredient in recipe.items():
-    print("in", ingredient[0])
-    print(ingredients)
-    if ingredients[ingredient[0]] and :
-      print("yes", ingredients[ingredient[0]])
-  # for myingredient in ingredients.items():
-  #   print(myingredient)
+  can_make = True
+  while can_make == True:
+    can_make = True
+    for ingredient in recipe.items():
+      # print("in", ingredient[0], "recipe", recipe[ingredient[0]])
+      # print(ingredients)
+      # print("ingred", ingredient[0], ingredients[ingredient[0]])
+      print(hasattr(ingredients, ingredient[0]))
+      print("ingredients", ingredients,ingredients[ingredient[0]], ingredient[0])
+      if hasattr(ingredients, ingredient[0]) and ingredients[ingredient[0]] >= recipe[ingredient[0]]:
+        print("yes", ingredients[ingredient[0]], recipe[ingredient[0]])
+        ingredients[ingredient[0]] =  ingredients[ingredient[0]] - recipe[ingredient[0]]
+      else:
+        print("No more")
+        can_make = False
+        return batches
+        print("Im here")
+    batches += 1
+    # for myingredient in ingredients.items():
+    #   print(myingredient)
+
 
 # recipe_batches({ 'milk': 100, 'butter': 50, 'flour': 5 }, { 'milk': 138, 'butter': 48, 'flour': 51 })
+{ 'milk': 100, 'butter': 50, 'cheese': 10 }, { 'milk': 198, 'butter': 52, 'cheese': 10 }
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'cheese': 10 }
+  ingredients = { 'milk': 198, 'butter': 52, 'cheese': 10 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
