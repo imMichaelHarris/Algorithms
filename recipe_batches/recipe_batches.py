05f20aa92d5c7875e10fcbb4762af8d64cac5cc2
Michael Harris <mhguitar1997@gmail.com>
2019-11-07 01:55:58 -0800

    Commiting work to continue on laptop

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # Both recipe and ingredients are dictionaries
+  # Both will have the same foem 
+  # Recipe is amount needed to complete
+  # Ingredients is amount we have
+  # Return the amount of batches we can make with the given ingredients
+  # print("recipe", recipe.items())
 
+  #
+  batches = 0
+  print(recipe.items())
+  for ingredient in recipe.items():
+    print("in", ingredient[0])
+    print(ingredients)
+    if ingredients[ingredient[0]] and :
+      print("yes", ingredients[ingredient[0]])
+  # for myingredient in ingredients.items():
+  #   print(myingredient)
+
+# recipe_batches({ 'milk': 100, 'butter': 50, 'flour': 5 }, { 'milk': 138, 'butter': 48, 'flour': 51 })
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
